0d88b202ee3684a0e4826a11f5183736c5895c61
伊東和成 <itokazunari5375@icloud.com>
2022-08-06 22:27:19 +0900

    一部変更

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,26 +3,19 @@ import type { NextPage } from "next";
 import { useEffect, useState } from "react";
 
 const Home: NextPage = () => {
-  const [value, setValue] = useState<string[]>(["apple", "grape", "orange"]);
-
-  const onClick = () => {
-    const copy = [...value];
-    copy.push("追加");
-    setValue(copy);
+  type Person = {
+    name: string;
+    age: number;
+    phone: string;
   };
-  useEffect(() => {
-    console.log("更新されました");
-    console.log(value);
-  }, [value]);
 
-  return (
-    <div>
-      {value.map((i, index) => (
-        <p key={index}>{i}</p>
-      ))}
-      <p onClick={onClick}>配列追加</p>
-    </div>
-  );
+  // type createdPick<T, K extends keyof T> = {
+  //   [P in K]: T[P];
+  // };
+
+  type newPerson = Exclude<Person, "age">;
+
+  return <div></div>;
 };
 
 export default Home;
